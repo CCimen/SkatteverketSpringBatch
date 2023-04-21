b31a8eee845b853ac4c56bbaa3ba97e463a251c2
CCimen <cagri.cimen.3264@student.uu.se>
2023-04-21 09:10:21 +0200

    Initial commit

---
+++ src/main/java/com/programvaruprojekt/skatteverketbatch/SkatteverketTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.programvaruprojekt.skatteverketbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkatteverketTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkatteverketTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programvaruprojekt/skatteverketbatch/SkatteverketTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programvaruprojekt.skatteverketbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkatteverketTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
